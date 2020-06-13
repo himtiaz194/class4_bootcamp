64550772a6cf5c468f304a22953e06b9f9efd16a
Hassan Imtiaz <h.imtiaz194@gmail.com>
2020-06-14 00:37:27 +0500

    build

--- src/message.js
+++ src/message.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Message(props) {
   return ( 
     <div>
-     <h1>Count value is:{props.counter }</h1>
+     <h1>Counter value is:{props.counter }</h1>
     </div>
   );
 }
